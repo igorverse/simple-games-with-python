9bfda3876dd105599edd397cad7fd39f0eef4e53
Igor Silva <osilvaigor@gmail.com>
2022-05-15 03:18:37 -0300

    WIP: add part of hangman game logic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+from random import randint
+from unicodedata import normalize
 
 
 def isLoginCorrect(login, password):
@@ -69,18 +71,146 @@ def registerUser(newUser, password):
 
 
 def hangmanGame():
-    raise Exception('Not implemented!')
+    """Summary or Description of the Function
+
+    Parameters:
+    argument1 (int): Description of arg1
+
+    Returns:
+    int:Returning value
+
+   """
+
+    with open('hangmanWords.json') as file:
+        words = json.load(file)
+
+    chooseTheme = input(
+        '1 - Heróis\n 2 - Vilões\n 3 - Nomes\n 4 - Animais\n 5 - Países\n Escolha uma opção de tema: ')
+    wordTheme = ''
+
+    if (chooseTheme == '1'):
+        wordTheme = 'heroes'
+    elif (chooseTheme == '2'):
+        wordTheme = 'vilains'
+    elif (chooseTheme == '3'):
+        wordTheme = 'names'
+    elif (chooseTheme == '4'):
+        wordTheme = 'animals'
+    elif (chooseTheme == '5'):
+        wordTheme = 'countries'
+    else:
+        raise Exception('Not implemented')
+
+    hangmanWord = (words[wordTheme][randint(
+        0, len(words[wordTheme]) - 1)]).lower()
+
+    treatedHangmanWord = normalize('NFKD', hangmanWord).encode(
+        'ascii', 'ignore').decode('ascii')
+
+    encryptedWord = ''
+
+    for letter in treatedHangmanWord:
+        if (letter != ' '):
+            encryptedWord += '_'
+        else:
+            encryptedWord += ' '
+
+    hangmanEvolution = ['''
+  *---*
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  *---*
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  *---*
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  *---*
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  *---*
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  *---*
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+    userMissesCounter = 0
+    userGuess = input('Insira uma letra: ')
+    while(not(userGuess.isalpha()) or len(userGuess) != 1):
+        print('\nJogada inválida!\n')
+        userGuess = input('Insira APENAS uma letra: ')
+
+    # while (userMissesCounter < 6 or encryptedWord != hangmanWord):
 
 
 def mazeGame():
+    """Summary or Description of the Function
+
+    Parameters:
+    argument1 (int): Description of arg1
+
+    Returns:
+    int:Returning value
+
+   """
     raise Exception('Not implemented!')
 
 
 def scoreboard():
+    """Summary or Description of the Function
+
+    Parameters:
+    argument1 (int): Description of arg1
+
+    Returns:
+    int:Returning value
+
+   """
     raise Exception('Not implemented!')
 
 
 def gamesMenu():
+    """Summary or Description of the Function
+
+    Parameters:
+    argument1 (int): Description of arg1
+
+    Returns:
+    int:Returning value
+
+   """
     chooseOption = input(
         '***************************************\nHora da diversão (ou não)!\n 1 - Jogo da Forca \n 2 - Labirinto InFEInal \n 3 - Placar dos Jogos\n Escolha uma opção: ')
 
